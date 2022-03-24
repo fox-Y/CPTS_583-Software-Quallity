f0435c7a9a108333152846741fce344c8aecd4ed
fox-Y <1270122172@qq.com>
2022-03-24 03:23:24 -0700

    delete a old version

--- Voting System/update a new version/Voting_system/table.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb as m
-
-db = m.connect(host = "localhost", user="root", passwd="mysql98", db="election")
-
-cur = db.cursor()
-
-cur.execute("Create table results (name varchar(50), adh varchar(12), age varchar(3), gender varchar(6), vote varchar(30))")
-cur.execute("commit;")
